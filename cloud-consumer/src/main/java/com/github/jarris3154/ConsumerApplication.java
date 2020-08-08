63e03759661f1ffc8232c1e72f81000a9c538387
Jarris3154 <hu.jia.jarris@qq.com>
2020-08-08 08:36:29 +0800

    chore: 更新

--- cloud-consumer/src/main/java/com/github/jarris3154/ConsumerApplication.java
+++ cloud-consumer/src/main/java/com/github/jarris3154/ConsumerApplication.java
@@ -2,10 +2,11 @@ package com.github.jarris3154;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
