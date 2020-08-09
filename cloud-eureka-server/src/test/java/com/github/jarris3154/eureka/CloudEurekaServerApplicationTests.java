2f2e074d3c2f5ef183af12f2cda97b429fd0d46f
Jarris3154 <hu.jia.jarris@qq.com>
2020-08-09 16:22:38 +0800

    chore: 修改默认配置

---
+++ cloud-eureka-server/src/test/java/com/github/jarris3154/eureka/CloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jarris3154.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
